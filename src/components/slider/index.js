d89a88ead8388be77c087c9f704302f5bdb41bb7
Diana Maduan <dianamaduan@MacBook-Air-Diana.local>
2020-11-15 16:36:04 +0600

    fixed-slider

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,23 +1,30 @@
 import React, {Component} from 'react'
 import connect from "react-redux/es/connect/connect";
-
+import Flickity from 'react-flickity-component'
+ 
 class Slider extends Component{
   
     render(){
       
-        // let slider = this.props.movies && this.props.movies.results ? this.props.movies.results.map(item => (
-        //     <div className="gallery-cell"><img src={'https://image.tmdb.org/t/p/original/'+item.backdrop_path} alt='images'></img></div>
-        // )):
-        // null
-       
+        let slider = this.props.movies && this.props.movies.results ? this.props.movies.results.map(item => (
+            <div className="gallery-cell"><img src={'https://image.tmdb.org/t/p/original/'+item.backdrop_path} alt='images'></img></div>
+        )):
+        null
+        const flickityOptions = {
+            initialIndex: 2
+        }
         return(
-        <div className="gallery js-flickity" data-flickity-options='{ "freeScroll": true, "wrapAround": true }'>
-            <div className="gallery-cell"><img src='https://image.tmdb.org/t/p/original/86L8wqGMDbwURPni2t7FQ0nDjsH.jpg' alt='images'></img></div>
-            <div className="gallery-cell"><img src='https://image.tmdb.org/t/p/original/wu1uilmhM4TdluKi2ytfz8gidHf.jpg' alt='images'></img></div>
-            <div className="gallery-cell"><img src='https://image.tmdb.org/t/p/original/8rIoyM6zYXJNjzGseT3MRusMPWl.jpg' alt='images'></img></div>
-            <div className="gallery-cell"><img src='https://image.tmdb.org/t/p/original/xoqr4dMbRJnzuhsWDF3XNHQwJ9x.jpg' alt='images'></img></div>
-        </div>
-         
+         <div>
+            <Flickity
+                className={'carousel'} 
+                elementType={'div'} 
+                options={flickityOptions} 
+                disableImagesLoaded={false} 
+                reloadOnUpdate 
+                >
+            {slider}
+            </Flickity>
+         </div>
         )
     }
 }
